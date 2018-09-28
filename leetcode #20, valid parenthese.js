20fc4b9c8029a9121a1ad79f82086ccb8cad9225
Danny Lu <dannylu8@outlook.com>
2018-09-28 16:34:30 -0700

    added comments

--- leetcode #20, valid parenthese.js
+++ leetcode #20, valid parenthese.js
@@ -30,13 +30,29 @@ console.log(isValid('([)]'))
 /*
 below is a working solution using stack
 
+example: '([])'
+
+1. store ( and [ because they both match           storage: ([
+2. see ], make sure the last one storage is [      storage: ([
+3. ok it's [, pop [                                storage: (
+4. see ), check the last one is (
+5. ok it's (, pop (                                sotrage: empty
+
+
+
 */
 
 const isValid = (string) => {
+    //created storage variable
 	let storage = [];
-	
+    
+    //iterate the entire string
 	for (let i = 0; i < string.length; i++){
-		if(string[i] === '(' || string[i] === '[' || string[i] === '{') storage.push(string[i]);
+
+        //push into stroage if '(' or '[' or '{'
+        if(string[i] === '(' || string[i] === '[' || string[i] === '{') storage.push(string[i]);
+        
+        //if see ')', and in storage, the last one has to be '('
 		else if (string[i] === ')' && storage[storage.length - 1] === '(') {
 			storage.pop();
 		}
@@ -45,7 +61,10 @@ const isValid = (string) => {
 		}
 		else if (string[i] === '}' && storage[storage.length - 1] === '{') {
 			storage.pop();
-		}
+        }
+        
+        //since the string only contain () [] {}, if it's not (, [, {
+        //like just ']', it will go straight to false because you can never start with ) ] }
 		else return false;
 	}
 	if(storage.length === 0) return true;
