592dd717ea0e06849b0c3e0511bf2c565bfe676e
Danny Lu <dannylu8@outlook.com>
2018-09-28 17:02:06 -0700

    ...

--- leetcode #21, merge two sorted linked list.js
+++ leetcode #21, merge two sorted linked list.js
@@ -1,27 +1,28 @@
-var mergeTwoLists = function(l1, l2) {
-    if (l1 === null) return l2
-    if (l2 === null) return l1
+const mergeTwoLists = (l1, l2) => {
     
-    let head = new ListNode(-1)
-    let current = head
-    
-    
-    while (l1 && l2) {
-        if (l1.val <= l2.val) {
-            current.next = l1
-            l1 = l1.next
-        } else {
-            current.next = l2
-            l2 = l2.next
-        }
-        
-        current = current.next
-    }
-    
-    while (l1 || l2) {
-        l1 ? (current.next = l1, l1 = l1.next) : (current.next = l2, l2 = l2.next)
-        current = current.next
-    }
-    
-    return head.next;
-};
+    let head = {
+         val:-1,
+         next:null
+     };
+     let current = head;
+     
+     
+     while (l1 && l2) {
+         if (l1.val <= l2.val) {
+             current.next = l1
+             l1 = l1.next
+         } else {
+             current.next = l2
+             l2 = l2.next
+         }
+         
+         current = current.next
+     }
+     
+     while (l1 || l2) {
+         l1 ? (current.next = l1, l1 = l1.next) : (current.next = l2, l2 = l2.next)
+         current = current.next
+     }
+     
+     return head.next;
+ };
