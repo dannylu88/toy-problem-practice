de5bb09a90e1564a39c9188ae9d2747b6da19b57
Danny Lu <dannylu8@outlook.com>
2018-09-30 22:47:55 -0700

    added forEach solution

--- leetcode #26, Remove Duplicates from Sorted Array.js
+++ leetcode #26, Remove Duplicates from Sorted Array.js
@@ -55,3 +55,16 @@ const removeDuplicates = (nums) => {
 
 
 console.log(removeDuplicates([1,1,3,3,4,4,5,5,5,5,5,5,5,6,7,8]))
+
+/*
+forEach also works
+*/
+var removeDuplicates = function(nums) {
+  nums.forEach((n, index) => {
+          while(n == nums[index +1]){
+              nums.splice(index +1, 1);
+          }
+   });
+  
+  return nums.length;
+};
