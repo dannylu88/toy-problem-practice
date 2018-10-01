55dca08c6a1d8d2b294a547fb816a8dfef89cb3f
Danny Lu <dannylu8@outlook.com>
2018-09-30 22:22:32 -0700

    added comments

--- leetcode #26, Remove Duplicates from Sorted Array.js
+++ leetcode #26, Remove Duplicates from Sorted Array.js
@@ -6,8 +6,10 @@ the below solution need to create a new array
 
 const removeDuplicates = (nums) => {
 	//create new var to store result
-	let result =[];
+  let result =[];
+  //iterate the for loop
 	for(let i = 0; i < nums.length; i++){
+    //if the result doesn't already have the number, push it in and store it
 		if (!result.includes(nums[i])){
 			result.push(nums[i])
 		}
@@ -16,4 +18,4 @@ const removeDuplicates = (nums) => {
 }
 
 
-console.log(removeDuplicates([1,1,3,3,4,4,5,5,5,5,5,5,5,6,7,8]))
+console.log(removeDuplicates([1,1,3,3,4,4,5,5,5,5,5,5,5,6,7,8])) // --> [1,3,4,5,6,7,8]
