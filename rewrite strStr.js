5a09e71121c2cd508ecb3cac444c4cad0573c5b4
Danny Lu <dannylu8@outlook.com>
2018-10-01 16:11:21 -0700

    added comment

--- rewrite strStr.js
+++ rewrite strStr.js
@@ -1,9 +1,21 @@
+
+
 const strStr = (haystack,needle) => {
-	if (needle.length === 0) return 0;
-	if(!haystack.includes(needle)) return -1;
-  for(let i = 0; i< haystack.length - needle.length; i++){
+    //check if contains the letter
+    if(!haystack.includes(needle)) return -1;
+    //some edge cases
+    if (needle.length === 0 || haystack.length === needle.length) return 0;
+    //iterate the haystack (longer string)
+    for(let i = 0; i< haystack.length; i++){
+
+        /*
+          string.substring(start index, end index -->not including end index)
+          hello.substring(0,2) --> he
+          
+          so here, when start iterating, check if the starting index + the needle length === the actual word needle
+        */
 	  if(haystack.substring(i,needle.length + i) === needle) return i;
    }	
 }
 
-console.log(strStr('hello','ll'))
+console.log(strStr('aaaaaaaa','baaa'))
