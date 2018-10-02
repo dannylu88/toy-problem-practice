3d929adbd10771858535ac677b2a98167d4b4620
Danny Lu <dannylu8@outlook.com>
2018-10-02 09:57:37 -0700

    solved problem with a for loop that read string from backwards

--- return last word.js
+++ return last word.js
@@ -15,3 +15,35 @@ const lengthOfLastWord = (string) =>{
 
 console.log(lengthOfLastWord('a'))
 
+/*
+O:length of the last word (string)
+I:a long string with white space
+C:N/A
+E:'Hello World' --> '5'
+  ' a ' ---> '1'
+
+*/
+
+const lengthOfLastWord = (s) =>{
+    //get rid of the white space both end of the string
+    let string = s.trim();
+    //set default length = 0 (if no word, return 0)
+    let length = 0;
+    //we start from the back of the string
+    //starting at the last letter, decrease 1 by 1, if i>=0, we keep going
+	for(let i = string.length - 1; i >= 0; i--){
+        //stop the loop til we find the first space
+		if(string[i] === ' '){
+			break;
+        }
+        //if no space, we keep the conut++
+		else{
+			length++;
+		}
+    }
+    //return count
+	return length;
+}
+
+
+console.log(lengthOfLastWord('hello world'))
