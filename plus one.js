b4f46a315a649601338a03e7175ae53cb28f0edb
Danny Lu <dannylu8@outlook.com>
2018-10-02 22:53:32 -0700

    added solution that handle edges cases (spread operator)

--- plus one.js
+++ plus one.js
@@ -21,3 +21,70 @@ const plusOne = (digits) =>{
 
 console.log(plusOne([1,2,4]))
 
+/*
+the solution below handle edge cases
+
+[9,9] --> [1,0,0]
+[9] --> [1,0]
+
+using the 1st solution, it pop the last one then add one, push back to array,
+will become [9,10], or [10]
+
+*/
+
+const plusOne = (digits) =>{
+    //iterate from the end of the array
+	for(let i = digits.length - 1; i>=0; i--){
+        //if it's less than 9, 8+1 =9 , 7+1=8, they are fine, we don't need to touch the numbers at the front
+		if(digits[i] < 9){
+            //add one
+            digits[i]++;
+            //return the entire array
+			return digits;
+        }
+        //if it is = 9, then add 1 will be 10, so set the last one to be 0
+        //the loop will continue, to find the previous number that is not a 9, then add 1 in front
+		digits[i] = 0
+    }
+    //the above will handle [2,9,9]  --> [3,0,0] because it will find the number in front of 9,9 then add 1 become 3
+    //however, for [9,9] or [9,9,9], there is no number in front of 9,9
+    //so when the for loop ends, and it reaches the front, if no number found,
+    //we add a one in front to make it [1,0,0] and [1,0,0,0]
+    return [1,...digits]
+    
+    /*
+     spread operator
+
+     1. replace apply:
+        function myFunction(x, y, z) { }
+        var args = [0, 1, 2];
+        myFunction.apply(null, args);
+
+        function myFunction(x, y, z) { }
+        var args = [0, 1, 2];
+        myFunction(...args);
+
+
+      2. insert into array
+      var parts = ['shoulders', 'knees']; 
+        var lyrics = ['head', ...parts, 'and', 'toes']; 
+        // ["head", "shoulders", "knees", "and", "toes"]
+
+      3. copy array
+      var arr = [1, 2, 3];
+var arr2 = [...arr]; // like arr.slice()
+arr2.push(4); 
+
+// arr2 becomes [1, 2, 3, 4]
+// arr remains unaffected
+
+    4.concat array
+    var arr1 = [0, 1, 2];
+var arr2 = [3, 4, 5];
+arr1 = [...arr2, ...arr1]; // arr1 is now [3, 4, 5, 0, 1, 2]
+    */
+}
+
+
+console.log(plusOne([1,2,4]))
+
