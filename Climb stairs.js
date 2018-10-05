17ddf358c77e3361e17cbe2a07d3b51a2eadf806
Danny Lu <dannylu8@outlook.com>
2018-10-05 14:00:11 -0700

    solved

--- Climb stairs.js
+++ Climb stairs.js
@@ -1,1 +1,16 @@
-const climbstar
+function climbStairs (n) {
+    // Write your code here, and
+    // return your final answer.
+    var counter = 3;
+    var ways = [];
+    ways[0] = 0; //0
+    ways[1] = 1; //1
+    ways[2] = 2; //2
+  
+    while (counter <= n)
+    {
+      ways[counter] = ways[counter -1] + ways[counter -2];
+      counter ++;
+    }
+    return ways[n];
+  }
