04be796efa76602a758a6ab2fa9ee122a3c88a86
Danny Lu <dannylu8@outlook.com>
2018-10-06 15:53:34 -0700

    added comment

--- same tree.js
+++ same tree.js
@@ -1,5 +1,8 @@
 const isSameTree = (p,q) => {
-	if(p===q) return true;
-	if(p===null || q===null) return false;
+    //p and q is the start nodes
+    if(p===q) return true;
+    //if pointer doesn't reach the end of the tree, keep going
+    if(p===null || q===null) return false;
+    //recursion check if has reached the end of the tree and recursion
 	return p.val===q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
