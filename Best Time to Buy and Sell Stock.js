f25cca39d136157ac766027e35c22424f31527d4
Danny Lu <dannylu8@outlook.com>
2018-10-10 23:33:17 -0700

    added comments

--- Best Time to Buy and Sell Stock.js
+++ Best Time to Buy and Sell Stock.js
@@ -8,9 +8,16 @@ E:[7,1,5,3,6,4]   --> 5, you buy when it's 1, and sell it at 6
 
 
 const testing = (prices) => {
-	
-	console.log(prices)
-	//console.log(original)
-	if(JSON.stringify(prices) === JSON.stringify([...prices].sort((a,b) => b-a))) return false;
-	else return Math.max(...prices) - Math.min(...prices);
+  //1.sort by descending order
+  //2.if the original array is the same as the sorted by descending order
+  //  that means the stock has been going down for the entire day so no trade
+  //3. REMEMBER sort will change the original input,
+  //   to use sort without changing the original, use [...array].sort()
+  if(JSON.stringify(prices) === JSON.stringify([...prices].sort((a,b) => b-a))) return false;
+
+  //if not, return max - min
+  //this DOESN'T work because you can only sell after you buy
+  //now this solution will just find the largest difference, doesn't consider
+  //the buy and sell situation
+  else return Math.max(...prices) - Math.min(...prices);
 }
