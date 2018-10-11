a0f7d5e4d2aa9709bc4f1168f66d7cb0e7e3eaa1
Danny Lu <dannylu8@outlook.com>
2018-10-10 22:51:15 -0700

    analyzed the problem using OICE

---
+++ Best Time to Buy and Sell Stock.js
@@ -0,0 +1,8 @@
+/*
+O:number(maximum profit)
+I:array of trades
+C:N/A
+E:[7,1,5,3,6,4]   --> 5, you buy when it's 1, and sell it at 6
+   [7,6,4,3,1]   ---> you don't buy because you will lose money
+*/
+
