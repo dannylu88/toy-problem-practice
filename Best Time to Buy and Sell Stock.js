a118b1a9930128173ffa3f32421f3aa380489237
Danny Lu <dannylu8@outlook.com>
2018-10-10 23:30:01 -0700

    solved half problem, only find max-min but neglect you can only sell after you buy

--- Best Time to Buy and Sell Stock.js
+++ Best Time to Buy and Sell Stock.js
@@ -6,3 +6,11 @@ E:[7,1,5,3,6,4]   --> 5, you buy when it's 1, and sell it at 6
    [7,6,4,3,1]   ---> you don't buy because you will lose money
 */
 
+
+const testing = (prices) => {
+	
+	console.log(prices)
+	//console.log(original)
+	if(JSON.stringify(prices) === JSON.stringify([...prices].sort((a,b) => b-a))) return false;
+	else return Math.max(...prices) - Math.min(...prices);
+}
