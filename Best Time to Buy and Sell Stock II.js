ba153f64c0b1ee605be3a5bc2d6941505ee81347
Danny Lu <dannylu8@outlook.com>
2018-10-11 21:34:00 -0700

    started new problem

---
+++ Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,9 @@
+/*
+O:maximum profit (you can buy and sell multiple times)
+I:array of numbers
+C:N/A
+E:[7,1,5,3,6,4] --> buy one day2, 1..sell when 5, 5 - 1 =4
+                --> buy one day4, 3, sell when 6, 6 - 3 = 3
+                sum = 4 + 3 = 7
+
+*/
