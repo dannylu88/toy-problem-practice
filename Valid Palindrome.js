09f6d22043c4d47ec7c67d3d7414b401c1b2e8c7
Danny Lu <dannylu8@outlook.com>
2018-10-14 22:50:22 -0700

    analyzed problem using OICE

---
+++ Valid Palindrome.js
@@ -0,0 +1,15 @@
+/*
+O:Boolean
+I:String (including puncutation, number, etc
+C: only consider characters, ignore cases, and empty string is valid
+E: 
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+*/
