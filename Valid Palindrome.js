5ae3b99ee4d066af48e0ccd75c2f0cf82c6d6f34
Danny Lu <dannylu8@outlook.com>
2018-10-16 23:52:50 -0700

    solved without using REGEXP.

--- Valid Palindrome.js
+++ Valid Palindrome.js
@@ -13,3 +13,32 @@ Input: "race a car"
 Output: false
 
 */
+
+var isPalindrome = function(s) {
+    if (s.length < 2) return true;
+    
+    const isCharValid = char => {
+      return (char >= 'a' && char <= 'z'
+         || char >= 'A' && char <= 'Z'
+         || char >= '0' && char <= '9');
+    };
+        
+    let i=0;
+    let k=s.length-1;
+    while (i<=k) {
+        if (!isCharValid(s[i])) {
+            i++;
+        }
+        else if (!isCharValid(s[k])) {
+            k--;  
+        } 
+        else if (s[i].toLowerCase() != s[k].toLowerCase()) {
+            return false;
+        } else {
+            i++;
+            k--;
+        }
+    }
+    
+    return true;
+};
