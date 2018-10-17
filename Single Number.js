b9798b585138804d22fbf15e847ff9ce36d18872
Danny Lu <dannylu8@outlook.com>
2018-10-17 14:57:52 -0700

    solved problem and satified the linear time and single memory constraint

--- Single Number.js
+++ Single Number.js
@@ -3,5 +3,22 @@ O:number, the single number that never repeat
 I:array of random intergers
 C:linear run time, no extra memory
 E:[2,2,1] -> 1;  [4,1,2,1,2] --> 4
+  [1,2,2] --> 1
+  [1,1,3,3,4,5,5]  -> 4
+  [1,1,3,3,5] --> 5
 */
+
+// sort number by order (this will change the original input)
+
+// the number must meet two condition, 1. next number is not the same, 2.next next number also not the same
+// the for loop will need to skip 2 index a time
+
+const singleNumber = (nums) => {
+	nums.sort((a,b) =>  a - b);
+	for(let i = 0; i<nums.length; i = i + 2){
+		if(nums[i] !== nums[i+1] && nums[i] !== nums[i+2]) return nums[i]
+	}
+}
+
+console.log(singleNumber([2,2,3,5,5]))
 
