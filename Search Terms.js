9a6dd84ae58f27fd02cd2c911710f4545b4eb21c
Danny Lu <dannylu8@outlook.com>
2018-10-18 21:27:05 -0700

    re-analyze the problem using OICE

--- Search Terms.js
+++ Search Terms.js
@@ -1,5 +1,9 @@
 /*
-
+O:index of the match words
+I:array of random words
+C:N/A
+E:  ['hey', 'hi', 'hello', 'hey', 'greetings', 'salutations', 'hi', 'hello', 'hey']
+    find ['hey','hi','hello']  --> return [0,1,6]
 
 */
 
