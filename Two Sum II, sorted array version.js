b4a58716fb110fa742e6678371337693ed9f66b2
Danny Lu <dannylu8@outlook.com>
2018-10-20 09:59:25 -0700

    analyzed problem using OICE

---
+++ Two Sum II, sorted array version.js
@@ -0,0 +1,9 @@
+/*
+O:indice of 2 element that add up to a specific sum
+I:sorted array
+C:non-zero base index, it's just regular index + 1
+E:Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+*/
+
