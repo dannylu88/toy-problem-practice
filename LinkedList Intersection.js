6a5871b6b513fd52280239eac3b17c4e6bbd1957
Danny Lu <dannylu8@outlook.com>
2018-10-20 09:52:39 -0700

    added detailed comments

--- LinkedList Intersection.js
+++ LinkedList Intersection.js
@@ -27,27 +27,47 @@ const getIntersectionNode = (headA, headB) => {
 	if(!headA || !headB) return null;
 	
 	let hA = headA;
-	let hB = headB;
+    let hB = headB;
+    //we won't use count here, but it's good to know
 	let count = 0;
-	
+    
+    //this loop is to find the length of the shorter linked list
 	while(hA && hB) {
+        //while either one of them exist, keep going on
 		hA = hA.next;
 		hB = hB.next;
 		count++;
 	}
 	//if hA is shorter, which means after the above loop, hA reach the end
 	if(hA === null) {
+        //so as long as the pointer of B exist, we keep moving
 		while(hB) {
+            /*
+              here is a little bit tricky part
+
+              now we know A is shorter because hA = null, the pointer reach til A's end
+
+              in order to find the intersection, we need to find the difference of the lengths
+
+              example: A has length of 5, B has length of 10, now we finish iterating A,
+                       the pointer at A is right at the intersection, but the pointer at B is only at 10 - 5 = 5,
+                       so we need B to reach its end too
+
+                       to do that, we start from headB, the original headB, NOT the pointer, 
+                       and we move the pointer by 5, so A and B has the same length
+            */
 			hB = hB.next;
 			headB = headB.next;
 		}
-	}
+    }
+    //case if hB is shorter
 	else if(hB === null){
 		while(hA) {
 			hA = hA.next;
 			headA = headA.next;
 		}
-	}
+    }
+    //now we have the same length, so we can just check both together to see if anything equal
 	while(headA && headB) {
 		if(headA === headB) return headA;
 		headA = headA.next;
