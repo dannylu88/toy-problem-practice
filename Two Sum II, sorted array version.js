0fed3b1d0b7d301dd55734fcbbe7a570e7b4bcb2
Danny Lu <dannylu8@outlook.com>
2018-10-20 10:56:53 -0700

    solved with easy brutal force O(n)^2

--- Two Sum II, sorted array version.js
+++ Two Sum II, sorted array version.js
@@ -1,5 +1,5 @@
 /*
-O:indice of 2 element that add up to a specific sum
+O:indice of 2 element that add up to a specific sum, in an array
 I:sorted array
 C:non-zero base index, it's just regular index + 1
 E:Input: numbers = [2,7,11,15], target = 9
@@ -7,3 +7,19 @@ Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 */
 
+//brutal force solution---> REALLY SLOW!!! 628ms, 0.95%
+const twoSum = (numbers, target) => {
+	let indice = [];
+	for(let i = 0; i < numbers.length; i++){
+		for(let j = 1; j < numbers.length; j++){
+			if(i===j) continue;
+			if(numbers[i] + numbers[j] === target) {
+				indice.push(i+1);
+				indice.push(j+1);
+				return indice;
+			}
+		}
+	}
+}
+
+console.log(twoSum([1,2,3,4,4,9,56,90],8))
